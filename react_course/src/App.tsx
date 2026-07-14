f7a26949566f3cdffe27031b5cc25ffed8d35a48
ArsenyoP <arsenyo198510@gmail.com>
2026-07-14 22:44:26 +0300

    Innit commit

---
+++ react_course/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <div className="container header-inner">
+          <h1 className="logo">Mini blog</h1>
+          <a href="#">Home</a>
+          <a href="#">Post</a>
+          <a href="#">About</a>
+          <a href="#">Submit</a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
